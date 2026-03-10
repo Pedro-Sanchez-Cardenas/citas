7b847fc7e82f8aa42c1ad92c05a243447336005a
Pedro Sánchez Cárdenas <pedrosanchezcardenas@MacBook-Pro-de-Pedro.local>
2026-03-09 20:03:43 -0600

    devcontainer

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,15 +1,7 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack(config, { dev }) {
-    if (dev) {
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-      };
-    }
-    return config;
-  },
-};
+  experimental: {
+    turbo: {}
+  }
+}
 
-export default nextConfig;
+export default nextConfig
